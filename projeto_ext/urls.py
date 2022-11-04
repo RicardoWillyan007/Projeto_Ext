65869000c83de1875a26b5ae3f48eb0b221c51be
Emilly Ribeiro de Souza Oliveira <emillyribeirosouza123@hotmail.com>
2022-11-04 14:17:07 -0300

    urls e views

--- projeto_ext/urls.py
+++ projeto_ext/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import perfil
 
 urlpatterns = [
+    path('perfil/', perfil, name='perfil')
     path('admin/', admin.site.urls),
 ]
