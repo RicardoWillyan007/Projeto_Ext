0ba803ceefd31ede17436eb644f7787e82c283e2
Emilly Ribeiro de Souza Oliveira <emilly.oliveira@academico.ifrn.edu.br>
2022-12-15 11:37:26 -0300

    adicionei campos em models

--- core/models.py
+++ core/models.py
@@ -5,6 +5,9 @@ class Usuario(AbstractUser):
     matricula = models.CharField('matricula', max_length=15, unique=True)
     nome = models.CharField('nome', max_length=100)
     idade = models.IntegerField('idade')
+    nascimento = models.DateField('nascimento')
+    telefone = models.CharField('telefone', max_length=100)
+    cpf = models.CharField('cpf', max_length=100)
 
     USERNAME_FIELD = 'matricula'
 
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .forms import UsuarioForm
+from .forms import UsuarioForm 
 
 def login(request):
     return render(request, 'login.html')
