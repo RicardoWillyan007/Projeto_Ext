eeaa939f29defa65c29583fb422881e989f457e7
RicardoWillyan007 <ricardowillyan2016@gmail.com>
2022-12-29 07:47:59 -0300

    correção em forms

--- core/models.py
+++ core/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
-    matricula = models.CharField('matricula', max_length=15, unique=True)
+    username = models.CharField('matricula', max_length=15, unique=True)
     nome = models.CharField('nome', max_length=100)
-    idade = models.IntegerField('idade')
     nascimento = models.DateField('nascimento')
     telefone = models.CharField('telefone', max_length=100)
     cpf = models.CharField('cpf', max_length=100)
 
-    USERNAME_FIELD = 'matricula'
+    USERNAME_FIELD = 'username'
 
